bdb52f3392de3fcab055283220af10ae5a16f7bb
Rohith <60003721+rohith678@users.noreply.github.com>
2023-05-28 21:12:26 +0530

    Update repository.dart
    adding correct db path and added functions for storage

--- lib/repository.dart
+++ lib/repository.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebaseapp/task.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -8,9 +9,16 @@ import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class FirebaseRepository {
   final _db = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
 
   void addTaskToDB(BuildContext context, Task task) async {
-    await _db.collection("tasks").doc(task.id).set(task.toJson()).then((value) {
+    await _db
+        .collection("users")
+        .doc(_auth.currentUser!.uid)
+        .collection("tasks")
+        .doc(task.id)
+        .set(task.toJson())
+        .then((value) {
       ScaffoldMessenger.of(context)
           .showSnackBar(const SnackBar(content: Text("Added successfully")));
     }).catchError((error) {
@@ -19,7 +27,12 @@ class FirebaseRepository {
   }
 
   Stream<List<Task>> getTasksStream() {
-    return _db.collection("tasks").snapshots().map((QuerySnapshot snapshot) {
+    return _db
+        .collection("users")
+        .doc(_auth.currentUser!.uid)
+        .collection("tasks")
+        .snapshots()
+        .map((QuerySnapshot snapshot) {
       return snapshot.docs.map((DocumentSnapshot document) {
         return Task(
             title: document['title'],
@@ -30,7 +43,13 @@ class FirebaseRepository {
   }
 
   void updateTask(BuildContext context, Task task) {
-    _db.collection("tasks").doc(task.id).update(task.toJson()).then((value) {
+    _db
+        .collection("users")
+        .doc(_auth.currentUser!.uid)
+        .collection("tasks")
+        .doc(task.id)
+        .update(task.toJson())
+        .then((value) {
       ScaffoldMessenger.of(context)
           .showSnackBar(const SnackBar(content: Text("Updated successfully")));
     }).catchError((error) {
@@ -40,8 +59,13 @@ class FirebaseRepository {
 
   void deleteTask(BuildContext context, Task task) {
     try {
-      _db.collection("tasks").doc(task.id).delete().then((value) =>
-          ScaffoldMessenger.of(context).showSnackBar(
+      _db
+          .collection("users")
+          .doc(_auth.currentUser!.uid)
+          .collection("tasks")
+          .doc(task.id)
+          .delete()
+          .then((value) => ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text("Deleted successfully"))));
     } catch (error) {
       debugPrint("Error in deleteTask $error");
@@ -50,6 +74,8 @@ class FirebaseRepository {
 
   Stream<List<Task>> serachByName(String name) {
     return _db
+        .collection("users")
+        .doc(_auth.currentUser!.uid)
         .collection("tasks")
         .where('title', isGreaterThanOrEqualTo: name)
         .where('title', isLessThanOrEqualTo: name)
@@ -69,14 +95,41 @@ class FirebaseRepository {
       firebase_storage.UploadTask? uploadTask;
       firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
           .ref()
-          .child("images")
+          .child("images/")
+          .child(_auth.currentUser!.uid)
           .child("/${file.name}");
       uploadTask = ref.putFile(File(file.path));
       await uploadTask.whenComplete(() => null);
       String ImageURL = await ref.getDownloadURL();
+      addImageToDB(ImageURL);
       debugPrint("ImageURL $ImageURL");
     } catch (error) {
       debugPrint("error in uploadImage $error");
     }
+  }
+
+  void addImageToDB(String downloadURL) async {
+    await _db
+        .collection("users")
+        .doc(_auth.currentUser!.uid)
+        .collection("images")
+        .add({"imageURL": downloadURL})
+        .then((value) {})
+        .catchError((error) {
+          debugPrint("Error in addData $error");
+        });
+  }
+
+  Stream<List<String>> getImageStream() {
+    return _db
+        .collection("users")
+        .doc(_auth.currentUser!.uid)
+        .collection("images")
+        .snapshots()
+        .map((QuerySnapshot snapshot) {
+      return snapshot.docs.map((DocumentSnapshot document) {
+        return document["imageURL"].toString();
+      }).toList();
+    });
   }
 }
